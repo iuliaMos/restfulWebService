ef6650061eaca890439affa04a19086a7f3e7200
moscalenco.iulia@gmail.com <aspaRagus45^!>
2021-02-15 12:52:06 +0200

    restconsumer

---
+++ src/main/java/com/example/ConsumingRestConfiguration.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import com.example.consumingrest.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConsumingRestConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumingRestConfiguration.class);
+
+    @Value("${quotation.url}")
+    private String quotationUrl;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(quotationUrl, Quote.class);
+            log.info(quote.toString());
+        };
+    }
+}
--- src/main/java/com/example/greeting/Greeting.java
+++ src/main/java/com/example/greeting/Greeting.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.greeting;
 
 public class Greeting {
 
